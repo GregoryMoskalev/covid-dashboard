000e03fd419e48f3f1acc3fa330519010339dd53
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-12-08 21:50:12 +0300

    init: start covid-dashboard task

---
+++ covid-dashboard/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const miniCss = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js'
+  },
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 5500
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'COVID-19 Dashboard',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new ESLintPlugin(),
+    new miniCss({
+      filename: 'main.css'
+    })
+  ],
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [ 'babel-loader' ]
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [ miniCss.loader, 'css-loader', 'sass-loader' ]
+      }
+    ]
+  }
+};
