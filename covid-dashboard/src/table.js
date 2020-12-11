c9c5fc303856fff38b893fc0e8d6223d2aec0f61
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-12-11 19:10:15 +0300

    feat: render global table + basic style

--- covid-dashboard/src/table.js
+++ covid-dashboard/src/table.js
@@ -3,8 +3,57 @@ import fetchData from './fetchData.js';
 
 export default class table {
   constructor() {
+    [ this.app ] = document.body.getElementsByClassName('app');
     this.fetchData = fetchData;
-    this.totalCases = null;
+    this.totalTable = { cases: null, deaths: null, recovered: null };
+    this.totalTableHeader = {};
+  }
+
+  renderControl() {
+    this.radioWrapper = document.createElement('div');
+    this.radioWrapper.classList.add('control');
+    this.app.appendChild(this.radioWrapper);
+    Object.keys(this.totalTable).forEach((bookmark) => {
+      const tableControl = document.createElement('a');
+      tableControl.classList.add('control__item');
+      tableControl.href = `#${bookmark}`;
+      tableControl.innerHTML = bookmark;
+      this.radioWrapper.appendChild(tableControl);
+    });
+  }
+
+  renderTable() {
+    this.table = document.createElement('div');
+    this.table.classList.add('table');
+    this.app.appendChild(this.table);
+
+    // всего
+    Object.keys(this.totalTable).forEach((bookmark) => {
+      this.totalTable[bookmark] = document.createElement('div');
+      this.totalTable[bookmark].classList.add(`table__total__${bookmark}`, 'table__total');
+      this.totalTable[bookmark].id = bookmark;
+      this.table.appendChild(this.totalTable[bookmark]);
+      this.totalTableHeader[bookmark] = document.createElement('h3');
+      this.totalTableHeader[bookmark].classList.add(`table__total__${bookmark}__header`);
+      this.totalTableHeader[bookmark].innerHTML = `Global ${bookmark}: ${this.sumData(bookmark)}`;
+      this.totalTable[bookmark].appendChild(this.totalTableHeader[bookmark]);
+      this.data.forEach((item) => {
+        const country = document.createElement('p');
+        country.innerHTML = `${item.country} <br> ${bookmark}: ${item[bookmark]}`;
+        this.totalTable[bookmark].appendChild(country);
+      });
+      console.log(this.totalTable);
+    });
+    // (() => {
+    //   this.todayTable = document.createElement('div');
+    //   this.todayTable.classList.add('table__today');
+    //   this.table.appendChild(this.totalTable);
+    //   this.data.forEach((item) => {
+    //     const country = document.createElement('p');
+    //     country.innerHTML = `${item.country} <br> cases: ${item.todayCases} deaths: ${item.todayDeaths} recovered: ${item.todayRecovered}`;
+    //     this.todayTable.appendChild(country);
+    //   });
+    // })();
   }
 
   sumData(param) {
@@ -20,34 +69,36 @@ export default class table {
     this.todayCases = this.sumData('todayCases');
     this.todayDeaths = this.sumData('todayDeaths');
     this.todayRecovered = this.sumData('todayRecovered');
-    const mod = this.sumData('population') / 100000;
-    console.table(this.data);
+    this.mod = this.sumData('population') / 100000;
+    this.renderControl();
+    this.renderTable();
+    // console.table(this.data);
     // общее количество случаев заболевания
-    console.log('cases', this.totalCases);
-    // общее количество летальных исходов
-    console.log('deaths', this.totalDeaths);
-    // // общее количество выздоровевших
-    console.log('recovered', this.totalRecovered);
+    // console.log('cases', this.totalCases);
+    // // общее количество летальных исходов
+    // console.log('deaths', this.totalDeaths);
+    // // // общее количество выздоровевших
+    // console.log('recovered', this.totalRecovered);
 
-    // // количество случаев заболевания за последний день
-    console.log('todayCases', this.todayCases);
-    // // количество летальных исходов за последний день
-    console.log('todayDeaths', this.todayDeaths);
-    // // количество выздоровевших за последний день
-    console.log('todayRecovered', this.todayRecovered);
+    // // // количество случаев заболевания за последний день
+    // console.log('todayCases', this.todayCases);
+    // // // количество летальных исходов за последний день
+    // console.log('todayDeaths', this.todayDeaths);
+    // // // количество выздоровевших за последний день
+    // console.log('todayRecovered', this.todayRecovered);
 
-    // // общее количество случаев заболевания из расчёта на 100 тыс. населения
-    console.log('cases per 100000', (this.totalCases / mod).toFixed(2));
-    // // общее количество летальных исходов из расчёта на 100 тыс. населения
-    console.log('deaths per 100000', (this.totalDeaths / mod).toFixed(2));
-    // // общее количество выздоровевших из расчёта на 100 тыс. населения
-    console.log('recovered per 100000', (this.totalRecovered / mod).toFixed(2));
+    // // // общее количество случаев заболевания из расчёта на 100 тыс. населения
+    // console.log('cases per 100000', (this.totalCases / this.mod).toFixed(2));
+    // // // общее количество летальных исходов из расчёта на 100 тыс. населения
+    // console.log('deaths per 100000', (this.totalDeaths / this.mod).toFixed(2));
+    // // // общее количество выздоровевших из расчёта на 100 тыс. населения
+    // console.log('recovered per 100000', (this.totalRecovered / this.mod).toFixed(2));
 
-    // // количество случаев заболевания за последний день из расчёта на 100 тыс. населения
-    console.log('todayCases per 100000', (this.todayCases / mod).toFixed(2));
-    // // количество летальных исходов за последний день из расчёта на 100 тыс. населения
-    console.log('todayDeaths per 100000', (this.todayDeaths / mod).toFixed(2));
-    // // количество выздоровевших за последний день из расчёта на 100 тыс. населения
-    console.log('todayRecovered per 100000', (this.todayRecovered / mod).toFixed(2));
+    // // // количество случаев заболевания за последний день из расчёта на 100 тыс. населения
+    // console.log('todayCases per 100000', (this.todayCases / this.mod).toFixed(2));
+    // // // количество летальных исходов за последний день из расчёта на 100 тыс. населения
+    // console.log('todayDeaths per 100000', (this.todayDeaths / this.mod).toFixed(2));
+    // // // количество выздоровевших за последний день из расчёта на 100 тыс. населения
+    // console.log('todayRecovered per 100000', (this.todayRecovered / this.mod).toFixed(2));
   }
 }
