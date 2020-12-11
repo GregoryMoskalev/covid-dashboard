6ef4da183fed74540ab0947b82ee0a9288e3ec8a
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-12-11 17:11:04 +0300

    feat: fetching data for all countries then sum it

--- covid-dashboard/src/fetchData.js
+++ covid-dashboard/src/fetchData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import '@babel/polyfill';
 
 export default async function fetchData(country = null) {
-  let request = 'https://corona.lmao.ninja/v3/covid-19/all';
+  let request = 'https://corona.lmao.ninja/v3/covid-19/countries';
   if (country) {
     request = `https://corona.lmao.ninja/v3/covid-19/countries/${country}`;
   }
--- covid-dashboard/src/table.js
+++ covid-dashboard/src/table.js
@@ -4,39 +4,50 @@ import fetchData from './fetchData.js';
 export default class table {
   constructor() {
     this.fetchData = fetchData;
+    this.totalCases = null;
+  }
+
+  sumData(param) {
+    return Object.values(this.data).reduce((sum, country) => sum + country[param], 0);
   }
 
   async init() {
-    this.data = await this.fetchData();
+    this.data = await this.fetchData('');
+    this.totalCases = this.sumData('cases');
+    this.totalDeaths = this.sumData('deaths');
+    this.totalRecovered = this.sumData('recovered');
 
-    const mod = this.data.population / 100000;
+    this.todayCases = this.sumData('todayCases');
+    this.todayDeaths = this.sumData('todayDeaths');
+    this.todayRecovered = this.sumData('todayRecovered');
+    const mod = this.sumData('population') / 100000;
     console.table(this.data);
     // общее количество случаев заболевания
-    console.log('cases', this.data.cases);
+    console.log('cases', this.totalCases);
     // общее количество летальных исходов
-    console.log('deaths', this.data.deaths);
-    // общее количество выздоровевших
-    console.log('recovered', this.data.recovered);
+    console.log('deaths', this.totalDeaths);
+    // // общее количество выздоровевших
+    console.log('recovered', this.totalRecovered);
 
-    // количество случаев заболевания за последний день
-    console.log('todayCases', this.data.todayCases);
-    // количество летальных исходов за последний день
-    console.log('todayDeaths', this.data.todayDeaths);
-    // количество выздоровевших за последний день
-    console.log('todayRecovered', this.data.todayRecovered);
+    // // количество случаев заболевания за последний день
+    console.log('todayCases', this.todayCases);
+    // // количество летальных исходов за последний день
+    console.log('todayDeaths', this.todayDeaths);
+    // // количество выздоровевших за последний день
+    console.log('todayRecovered', this.todayRecovered);
 
-    // общее количество случаев заболевания из расчёта на 100 тыс. населения
-    console.log('cases per 100000', (this.data.cases / mod).toFixed(2));
-    // общее количество летальных исходов из расчёта на 100 тыс. населения
-    console.log('deaths per 100000', (this.data.deaths / mod).toFixed(2));
-    // общее количество выздоровевших из расчёта на 100 тыс. населения
-    console.log('recovered per 100000', (this.data.recovered / mod).toFixed(2));
+    // // общее количество случаев заболевания из расчёта на 100 тыс. населения
+    console.log('cases per 100000', (this.totalCases / mod).toFixed(2));
+    // // общее количество летальных исходов из расчёта на 100 тыс. населения
+    console.log('deaths per 100000', (this.totalDeaths / mod).toFixed(2));
+    // // общее количество выздоровевших из расчёта на 100 тыс. населения
+    console.log('recovered per 100000', (this.totalRecovered / mod).toFixed(2));
 
-    // количество случаев заболевания за последний день из расчёта на 100 тыс. населения
-    console.log('todayCases per 100000', (this.data.todayCases / mod).toFixed(2));
-    // количество летальных исходов за последний день из расчёта на 100 тыс. населения
-    console.log('todayDeaths per 100000', (this.data.todayDeaths / mod).toFixed(2));
-    // количество выздоровевших за последний день из расчёта на 100 тыс. населения
-    console.log('todayRecovered per 100000', (this.data.todayRecovered / mod).toFixed(2));
+    // // количество случаев заболевания за последний день из расчёта на 100 тыс. населения
+    console.log('todayCases per 100000', (this.todayCases / mod).toFixed(2));
+    // // количество летальных исходов за последний день из расчёта на 100 тыс. населения
+    console.log('todayDeaths per 100000', (this.todayDeaths / mod).toFixed(2));
+    // // количество выздоровевших за последний день из расчёта на 100 тыс. населения
+    console.log('todayRecovered per 100000', (this.todayRecovered / mod).toFixed(2));
   }
 }
