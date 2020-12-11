a754e2f99b75e0ac63bd42602aba3dac5a12a1cc
AlinaSh96 <alina30081996@gmail.com>
2020-12-12 00:46:37 +0300

    feat: add search

---
+++ covid-dashboard/src/search.js
@@ -0,0 +1,23 @@
+export default function liveSearch() {
+  
+  const input = document.querySelector('.list__search');
+  input.oninput = function () {
+    const value = this.value.trim()
+    const  items = document.querySelectorAll('.country')
+    if (input !== '') {
+      items.forEach((elem) => {
+        if (elem.innerHTML.search(value) === -1){
+          elem.classList.add('hide');
+        }
+        else {
+          elem.classList.remove('hide');
+        }
+      })
+    }
+    else {
+      items.forEach((elem) => {
+        elem.classList.remove('hide');
+      })
+    }
+  }
+}
