616f517ba2577beabe3160b964e3633bd7bcbe2a
AlinaSh96 <alina30081996@gmail.com>
2020-12-18 18:57:02 +0300

    feat: add customEvents

---
+++ covid-dashboard/src/getCountry.js
@@ -0,0 +1,23 @@
+const countryChoise = function (country) {
+  const event = new CustomEvent ( 'choiseCountry', {
+    detail: {
+      country,
+    },
+    bubbles: true,
+    cancelable: false,
+  });
+  document.dispatchEvent(event);
+  }
+
+function getCountryName(event) {
+  const wr = event.target.closest(".country");
+  const countryTitle = wr.querySelector(".country__title");
+  const country = countryTitle.innerHTML;
+  countryChoise(country)
+  return country;
+}
+
+export default function getCountry() {
+  const countryList = document.querySelector(".list__contries");
+  countryList.addEventListener("click", (event) => getCountryName(event));
+}
